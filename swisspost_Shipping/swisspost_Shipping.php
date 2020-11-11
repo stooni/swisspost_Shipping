12e199e904f96ab2634d35c0477437339254c58e
Martin Steiner <web@masteiner.ch>
2020-11-11 14:08:14 +0100

    Update swisspost_Shipping.php
    public function calculate_shipping( $package = array() )

--- swisspost_Shipping/swisspost_Shipping.php
+++ swisspost_Shipping/swisspost_Shipping.php
@@ -108,7 +108,7 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
                  * @param mixed $package
                  * @return void
                  */
-                public function calculate_shipping( $package ) {
+                 public function calculate_shipping( $package = array() ) {
                     
                     $weight = 0;
                     $cost = 0;
@@ -248,7 +248,7 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
                  * @param mixed $package
                  * @return void
                  */
-              public function calculate_shipping( $package ) {
+             public function calculate_shipping( $package = array() ) {
                     
                     $weight = 0;
                     $cost = 0;
@@ -388,7 +388,7 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
                  * @param mixed $package
                  * @return void
                  */
-              public function calculate_shipping( $package ) {
+              public function calculate_shipping( $package = array() ){
                     
                     $weight = 0;
                     $cost = 0;
